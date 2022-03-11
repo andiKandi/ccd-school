5f9e2a0dfbf1e08a0acf6ffeb367851bd07ca1fe
andiKandi <akraus931@gmail.com>
2022-03-11 10:30:17 +0100

    add christmasTree kata

---
+++ src/ccd/christmasTree.ts
@@ -0,0 +1,52 @@
+/*
+ * URL: https://ccd-school.de/coding-dojo/function-katas/tannenbaum/
+ * Name: Function Kata „Tannenbaum“
+ * Task: Schreibe eine Funktion, die einen Tannenbaum mit ASCII-Art „gezeichnet“ als Text zurückliefert.
+ * Eingabe ist die Höhe des Tannenbaums. Als Beispiel ein Tannenbaum der Höhe 5:
+       X
+      XXX
+     XXXXX
+    XXXXXXX
+   XXXXXXXXX
+       I
+ * */
+interface TreeLevel {
+  twigCount: number;
+  whitespaceCount: number;
+}
+export const christmasTree = (treeHeight: number) => {
+  const calculateTreeLevelValues = (treeHeight: number): TreeLevel[] => {
+    let twigCount: number = 1;
+    let whitespaceCount: number = treeHeight - 1;
+    const treeLevels: TreeLevel[] = [{ twigCount, whitespaceCount }];
+    for (let i = 1; i < treeHeight; i++) {
+      treeLevels.push({ twigCount: (twigCount += 2), whitespaceCount: (whitespaceCount -= 1) });
+    }
+    return treeLevels;
+  };
+
+  const treeLevels = calculateTreeLevelValues(treeHeight);
+
+  const drawTree = (treeLevels: TreeLevel[]): void => {
+    const drawTwig = (num: number): string => {
+      const twig = 'X';
+      return twig.repeat(num);
+    };
+
+    const drawWhitespace = (num: number): string => {
+      const whitespace = ' ';
+      return whitespace.repeat(num);
+    };
+
+    const drawStem = (num: number): string => {
+      const whitespace = ' ';
+      return `${whitespace.repeat(num + 1)}I`;
+    };
+
+    treeLevels.map((treeLevel) => {
+      console.log(drawWhitespace(treeLevel.whitespaceCount), drawTwig(treeLevel.twigCount));
+    });
+    console.log(drawStem(treeLevels[0].whitespaceCount));
+  };
+  drawTree(treeLevels);
+};
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { fizzbuzz } from './ccd/fizzbuzz';
 import { happyNumbers } from './ccd/happyNumbers';
 import { rotThirteen } from './ccd/rotThirteen';
 import { russianMultiplication } from './ccd/russianMultiplication';
+import { christmasTree } from './ccd/christmasTree';
 
 const index = (challengeNumber: number) => {
   switch (challengeNumber) {
@@ -32,8 +33,12 @@ const index = (challengeNumber: number) => {
       happyNumbers(previousNumbers, 19);
       break;
     }
+    case 7: {
+      christmasTree(5);
+      break;
+    }
     default:
       break;
   }
 };
-index(6);
+index(7);
